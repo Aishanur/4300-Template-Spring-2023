3c0906ee012e466cba19e91c1fc6d2de2c0c166d
Chrishana Wickham <clw89@cornell.edu>
2023-04-17 23:33:49 -0400

    second po4 change

--- backend/app.py
+++ backend/app.py
@@ -37,10 +37,18 @@ CORS(app)
 #     data = mysql_engine.query_selector(query_sql)
 #     return json.dumps([dict(zip(keys,i)) for i in data])
 
+# # there's a much better and cleaner way to do this
+# def sql_search(episode): #run the code below for every ingredient to get list of recipies that have ingredient. For loop for each ingredient that we get through episode input. Then get which recipies contain all of them
+#     query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
+#     keys = ["id","title","descr"] 
+#     data = mysql_engine.query_selector(query_sql)
+#     return json.dumps([dict(zip(keys,i)) for i in data])
+
 def sql_search(ingredient): #run the code below for every ingredient to get list of recipies that have ingredient. For loop for each ingredient that we get through episode input. Then get which recipies contain all of them
     ingredient_list = [i.strip() for i in ingredient.split(',')] # split text into a list of episode titles
-    query_sql = f"""SELECT * FROM recipes_reviews WHERE {' AND '.join([f"LOWER(RecipeIngredientParts) LIKE '%%{i.lower()}%%'" for i in ingredient_list])}"""
-    keys = ["ReviewId", "RecipeId", "ReviewAuthorId", "CurrentRating", "Review", "Name", "TotalTime", "DatePublished", "Description", "Image", "RecipeCategory", "Keywords", "RecipeIngredientQuantities", "RecipeIngredientParts", "ReviewCount", "Calories", "FatContent", "SaturatedFatContent", "CholesterolContent", "SodiumContent", "CarbohydrateContent", "FiberContent", "SugarContent", "ProteinContent", "RecipeInstructions", "AvgRecipeRating"]
+    # query_sql = f"""SELECT * FROM recipe_details WHERE {' AND '.join([f"LOWER(ingredients) LIKE '%%{i.lower()}%%'" for i in ingredient_list])}"""
+    query_sql = f"""SELECT * FROM recipe_details WHERE {' AND '.join([f"LOWER(ingredients) LIKE '%%{i.lower()}%%'" for i in ingredient_list])} ORDER BY ratings DESC"""
+    keys =  ["id", "title", "min", "ingredients", "ratings"]
     data = mysql_engine.query_selector(query_sql)
     return json.dumps([dict(zip(keys, i)) for i in data])
 
