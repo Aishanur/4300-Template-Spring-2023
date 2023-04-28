8e86b8d9455251a0bd34ed72b8f7e9954604fefc
Aditi Kashyap <67921890+aditikashyap@users.noreply.github.com>
2023-04-28 12:43:53 -0400

    Update rocchio.py

--- backend/rocchio.py
+++ backend/rocchio.py
@@ -5,7 +5,7 @@ import numpy as np
 #funct to convert query (list of ingredients) into np array of length y (total num unique ingredients in dataset)
 
 #rocchio algorithm
-def rocchio(query_vec, relevant, irrelevant, tfidf_matrix, a=.3, b=.3, c=.8, clip = True):
+def rocchio(query_vec, relevant, irrelevant, np_matrix, a=.3, b=.3, c=.8, clip = True):
     """
     Note that:
     X = total number of recipes in dataset
@@ -19,7 +19,7 @@ def rocchio(query_vec, relevant, irrelevant, tfidf_matrix, a=.3, b=.3, c=.8, cli
     relevant: list of np ingredients vectors of the recipes that the user has liked
     irrelevant: list of np ingredients vectors of the recipes that the user has disliked
 
-    tfidf_matrix: tfidf matrix with X rows (representing recipes) and Y columns (representing ingredients)
+    np_matrix: matrix with X rows (representing recipes) and Y columns (representing ingredients)
 
     Returns:
     rocc_q: modified query vector
@@ -27,8 +27,8 @@ def rocchio(query_vec, relevant, irrelevant, tfidf_matrix, a=.3, b=.3, c=.8, cli
 
     rocc_q = a * query_vec
 
-    agg_rel = np.zeros(len(tfidf_matrix[0]))
-    agg_irrel = np.zeros(len(tfidf_matrix[0]))
+    agg_rel = np.zeros(len(np_matrix[0]))
+    agg_irrel = np.zeros(len(np_matrix[0]))
     len_rel = len(relevant)
     len_irrel = len(irrelevant)
 
@@ -53,54 +53,61 @@ def rocchio(query_vec, relevant, irrelevant, tfidf_matrix, a=.3, b=.3, c=.8, cli
 
     return rocc_q
 
-
 #funct to output top 10 recipes with rocchio (cos-sim)
 def top10_with_rocchio(query_vec, relevant, irrelevant, tfidf_matrix, input_rocchio):
     """
     Calculates cos-sim between updated query vector and all recipes containing user's initial list of ingredients
     and the updated query vector.
-
     Ranks the similarities and returns 10 recipes with highest score
     The results do NOT include the recipes already marked relevant
-
     Parameters
     ----------
     query_vec : a numpy array of size 1 x Y, such that query_vec[i] = 1 if the user has inputted the 
     ith ingredient of the dataset into the query
-    
-    relevant: list of np ingredients vectors of the recipes that the user has liked
-    
-    irrelevant: list of np ingredients vectors of the recipes that the user has disliked
 
+    recipe_no: integer i where i indicates that this query represents the ith recipe (i.e. it is some valid index of 
+    the tfidf_matrix)
+    
+    relevant: list of pairs (recipe_no, ingredient vector) where recipe_no indicates the index of the recipe marked relevant,
+    and ingredient_vector is the corresponding np ingredient vector
+    
+    irrelevant: list of pairs (recipe_no, ingredient vector) where recipe_no indicates the index of the recipe marked irrelevant,
+    and ingredient_vector is the corresponding np ingredient vector    
+    
     tfidf_matrix: tfidf matrix with X rows (representing recipes) and Y columns (representing ingredients)
-
+    
     input_rocchio: Rocchio algorithm implemented above
-
+    
     Returns
     -------
-    dict
-        Returns the top ten highest ranked recipes for each query in the format described above.
+    rocc_rankings (dict)
+        Returns dict of 10 (recipe_no, ingredient vector) pairs which have highest cosine similarity with modified 
+        query vector and are NOT the recipes already marked relevant
+        
     """
     rocc_rankings = {}
 
-    for i in range(len(relevant)):
-        rocc_query = input_rocchio(query_vec, relevant, irrelevant, tfidf_matrix)
+    rocc_query = input_rocchio(query_vec, relevant, irrelevant, tfidf_matrix)
+    cos_sims = []
+
+    rel_recipe_nos = [rel[0] for rel in relevant]
+
+    for j in range(len(tfidf_matrix)):
+        #if this is a recipe already marked relevant, we don't want to return it in our new results
+        if j in rel_recipe_nos:
+            cos_sim.append(-1)
         
-        cos_sims = []
-        # for j in range(len(tfidf_matrix)):
-        #     if mov_ind == j:
-        #         cos_sims.append(-1)
-        #     elif mov_ind != j:
-        #         cos_sim = np.dot(rocc_query, input_doc_matrix[j])
-        #         cos_sims.append(cos_sim)
-            
-        
-        # mov_score_lst = [(movie_index_to_name[i], s) for i,s in enumerate(cos_sims)]
-        # mov_score_lst = mov_score_lst[:mov_ind] + mov_score_lst[mov_ind+1:]
-        # mov_score_lst = sorted(mov_score_lst, key=lambda x: -x[1])
-        
-        # rocc_rankings.update({mov_name : []})
-        # for name, score in mov_score_lst[:10]:
-        #     rocc_rankings[mov_name].append(name)
-    
-    return rocc_rankings 
+        #not a recipe marked relevant, so calculate the cos_sim
+        else:
+            cos_sim = np.dot(rocc_query, tfidf_matrix[j])
+            cos_sims.append(cos_sim)
+
+    #get indices of recipes with highest cos-sim scores
+    top10_recipe_nos = sorted(range(len(cos_sims)), key=lambda i: cos_sims[i], reverse=True)[:10]
+
+    for i in range(10):
+        #grab index of one of the top ten ranked recipes
+        recipe_no = top10_recipe_nos[i]
+        rocc_rankings.update({ recipe_no: tfidf_matrix[recipe_no]})
+
+    return rocc_rankings
