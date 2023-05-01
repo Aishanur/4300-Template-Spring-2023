ba01fc94a0998bccb7fb0a9d7dcd5537c532880f
mohammedoshikoya1 <oshikoya.m1@gmail.com>
2023-05-01 19:20:11 -0400

    updated text.py accuracy

--- backend/text.py
+++ backend/text.py
@@ -4,4 +4,4 @@ def remove_c_parantheses(text):
   """
   returns: text with the c() surrounding it removed
   """
-  return re.sub(r'c\((.*?)\)', r'\1', text)
+  return re.sub(r'c\(([^)]+)\)', r'\1', text)
